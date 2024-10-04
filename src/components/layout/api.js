98449c191fadcf11e98ceaf5397d2e32f871563a
Dominik Korniichuk <kordominik1@gmail.com>
2024-10-05 01:27:20 +0300

    add appsider component

---
+++ src/components/layout/api.js
@@ -0,0 +1,18 @@
+//Емулюю асинхронні запити, так як ліміт запитів обмежений, при потребі переробити на звичайний fetch
+import { cryptoAssets, cryptoData } from "../../data";
+
+export function fakeFetchCrypto() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(cryptoData);
+    }, 1);
+  });
+}
+
+export function fetchAssets() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(cryptoAssets);
+      }, 1);
+    });
+  }
